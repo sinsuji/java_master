ad7950163ff7f45a0894f42bdc838f155c112d7e
gram <gram@DESKTOP-K3N8OQE>
2023-11-29 21:41:26 +0900

    과제

--- hgjava/src/todo/p20231129/MemberService.java
+++ hgjava/src/todo/p20231129/MemberService.java
@@ -1,5 +1,23 @@
 package todo.p20231129;
 
-public class MemberService {
+import todo.p20231128.Board;
 
+public class MemberService {
+	String id;
+	String password;
+	
+	
+	boolean login(String id, String password) {
+		if(id.equals("hong") && password.equals("12345")) {
+			return true;
+		}else{
+			return false;
+		}
+		
+	}
+	
+	void logout(String id) {
+		System.out.println("로그아웃 되었습니다.");
+	}
+	
 }
